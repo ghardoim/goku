717d0555fc08cd056a1cea184e281089f58daa50
Gabriel Hardoim <ghardoim@hotmail.com>
2021-02-01 22:15:34 -0300

    alterações para automatizar processo de escrita no holyrics

--- src/friday/hands/mouse.py
+++ src/friday/hands/mouse.py
@@ -1,4 +1,13 @@
 import pyautogui as auto
+import time as time
 
 def vai_pro_canto():
   auto.moveTo(1, 1)
+
+def clica(x, y, tempo = 0):
+  time.sleep(tempo)
+  auto.click(x, y)
+
+def duplo_clique(x, y, tempo = 0):
+  time.sleep(tempo)
+  auto.doubleClick(x, y)
